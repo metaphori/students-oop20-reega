7e794587cfcff170f6344de885983d0f481a416d
Manuele <manuele.develop@gmail.com>
2021-01-19 20:15:56 +0100

    Fallback on contract start_time when getting the latest produced data time

--- src/main/java/reega/data/LocalDatabase.java
+++ src/main/java/reega/data/LocalDatabase.java
@@ -26,62 +26,63 @@ import reega.data.models.PriceModel;
 import reega.data.models.ServiceType;
 
 public final class LocalDatabase implements DataController {
-    private final DBAccess db;
+	private final DBAccess db;
 
-    protected LocalDatabase() throws ClassNotFoundException, SQLException {
-        this.db = new DBAccess().getInstance();
-    }
+	protected LocalDatabase() throws ClassNotFoundException, SQLException {
+		this.db = new DBAccess().getInstance();
+	}
 
-    @Override
-    public List<Contract> getUserContracts(final int userID) throws IOException, SQLException {
-        final String sql = String.format(this.db.getQuery("user_contracts.sql"), userID);
+	@Override
+	public List<Contract> getUserContracts(final int userID) throws IOException, SQLException {
+		final String sql = String.format(this.db.getQuery("user_contracts.sql"), userID);
 
-        final Statement s = this.db.getConnection().createStatement();
-        final ResultSet rs = s.executeQuery(sql);
-        final Type pricesType = new TypeToken<Map<String, Double>>() {
-        }.getType();
-        final Type servicesType = new TypeToken<List<String>>() {
-        }.getType();
-        final Calendar tzUTC = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-        final List<Contract> contracts = new ArrayList<>();
-        while (rs.next()) {
-            final Map<String, Double> prices = new Gson().fromJson(rs.getString("prices"), pricesType);
-            final PriceModel pm = new PriceModel(rs.getInt("price_model_id"), rs.getString("price_model_name"), prices);
-            final List<String> services = new Gson().fromJson(rs.getString("services"), servicesType);
-            contracts.add(new Contract(rs.getInt("contract_id"), rs.getInt("user_id"), rs.getString("address"),
-                    services, pm, new Date(rs.getTimestamp("start_time", tzUTC).getTime())));
-        }
-        rs.close();
-        s.close();
-        return contracts;
-    }
+		final Statement s = this.db.getConnection().createStatement();
+		final ResultSet rs = s.executeQuery(sql);
+		final Type pricesType = new TypeToken<Map<String, Double>>() {
+		}.getType();
+		final Type servicesType = new TypeToken<List<String>>() {
+		}.getType();
+		final Calendar tzUTC = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		final List<Contract> contracts = new ArrayList<>();
+		while (rs.next()) {
+			final Map<String, Double> prices = new Gson().fromJson(rs.getString("prices"), pricesType);
+			final PriceModel pm = new PriceModel(rs.getInt("price_model_id"), rs.getString("price_model_name"), prices);
+			final List<String> services = new Gson().fromJson(rs.getString("services"), servicesType);
+			contracts.add(new Contract(rs.getInt("contract_id"), rs.getInt("user_id"), rs.getString("address"),
+					services, pm, new Date(rs.getTimestamp("start_time", tzUTC).getTime())));
+		}
+		rs.close();
+		s.close();
+		return contracts;
+	}
 
-    @Override
-    public void putUserData(final Data data) throws SQLException {
-        final StringBuilder base = new StringBuilder(
-                "insert into data (\"type\",\"value\",\"contract_id\",\"timestamp\") values ");
+	@Override
+	public void putUserData(final Data data) throws SQLException {
+		final StringBuilder base = new StringBuilder(
+				"insert into data (\"type\",\"value\",\"contract_id\",\"timestamp\") values ");
 
-        for (final Entry<Long, Double> record : data.getData().entrySet()) {
-            final ZonedDateTime d = Instant.ofEpochMilli(record.getKey()).atZone(ZoneOffset.UTC);
-            base.append(String.format(Locale.US, "(%d, %f, %d, '%tF %tT'),", data.getType().getID(), record.getValue(),
-                    data.getContractID(), d, d));
-        }
-        // remove trailing ","
-        base.deleteCharAt(base.lastIndexOf(","));
-        this.db.executeStatement(base.toString());
-    }
+		for (final Entry<Long, Double> record : data.getData().entrySet()) {
+			final ZonedDateTime d = Instant.ofEpochMilli(record.getKey()).atZone(ZoneOffset.UTC);
+			base.append(String.format(Locale.US, "(%d, %f, %d, '%tF %tT'),", data.getType().getID(), record.getValue(),
+					data.getContractID(), d, d));
+		}
+		// remove trailing ","
+		base.deleteCharAt(base.lastIndexOf(","));
+		this.db.executeStatement(base.toString());
+	}
 
-    @Override
-    public Long getLatestData(final int contractID, final ServiceType service) throws SQLException {
-        final String sql = String.format(
-                "select \"timestamp\" from  data where \"contract_id\" = %d and \"type\" = %d order by \"timestamp\" desc limit 1;",
-                contractID, service.getID());
-        final Statement s = this.db.getConnection().createStatement();
-        final ResultSet rs = s.executeQuery(sql);
-        if (!rs.next()) {
-            return null;
-        }
-        final Calendar tzUTC = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-        return new Date(rs.getTimestamp("timestamp", tzUTC).getTime()).getTime();
-    }
+	@Override
+	public Long getLatestData(final int contractID, final ServiceType service) throws SQLException {
+		final String sql = String.format(
+				"with latestData as (select \"timestamp\" from  data where \"contract_id\" = %d and \"type\" = %d order by \"timestamp\" desc limit 1)"
+						+ "select coalesce((select * from latestData), (select start_time from contracts where id = %d))",
+				contractID, service.getID(), contractID);
+		final Statement s = this.db.getConnection().createStatement();
+		final ResultSet rs = s.executeQuery(sql);
+		if (!rs.next()) {
+			return null;
+		}
+		final Calendar tzUTC = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		return new Date(rs.getTimestamp("timestamp", tzUTC).getTime()).getTime();
+	}
 }
