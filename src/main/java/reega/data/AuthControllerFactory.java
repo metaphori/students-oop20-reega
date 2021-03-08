6ee9e9315c2d0f9e9b8eb09f758c4797abbb5969
Manuele <manuele.pola@studio.unibo.it>
2021-03-09 00:06:31 +0100

    Using remote as default db

--- src/main/java/reega/data/AuthControllerFactory.java
+++ src/main/java/reega/data/AuthControllerFactory.java
@@ -1,16 +1,16 @@
 package reega.data;
 
-import java.sql.SQLException;
-
 import reega.data.local.LocalAuth;
 import reega.data.remote.RemoteAuthAPI;
+
+import java.sql.SQLException;
 
 /**
  * This factory returns an implementation of AuthController based on the needs.
  */
 public final class AuthControllerFactory {
     public static AuthController getDefaultAuthController() throws ClassNotFoundException, SQLException {
-        return new LocalAuth();
+        return RemoteAuthAPI.getInstance();
     }
 
     public static AuthController getLocalAuthController() throws ClassNotFoundException, SQLException {
--- src/main/java/reega/data/DataControllerFactory.java
+++ src/main/java/reega/data/DataControllerFactory.java
@@ -1,16 +1,16 @@
 package reega.data;
 
-import java.sql.SQLException;
-
 import reega.data.local.LocalDatabase;
 import reega.data.remote.RemoteDatabaseAPI;
+
+import java.sql.SQLException;
 
 /**
  * This factory returns an implementation of DataController based on the needs.
  */
 public final class DataControllerFactory {
     public static DataController getDefaultDataController() throws ClassNotFoundException, SQLException {
-        return new LocalDatabase();
+        return RemoteDatabaseAPI.getInstance();
     }
 
     public static DataController getLocalDatabaseController() throws ClassNotFoundException, SQLException {
