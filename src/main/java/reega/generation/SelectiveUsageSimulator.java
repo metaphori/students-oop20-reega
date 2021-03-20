682caab76b4e7485ba051bdc98e4c49bb77b7b86
Daniel Benazzi <daniel.benazzi@studio.unibo.it>
2021-03-20 19:10:46 +0100

    update UsageSimulator

--- src/main/java/reega/generation/SelectiveUsageSimulator.java
+++ src/main/java/reega/generation/SelectiveUsageSimulator.java
@@ -6,37 +6,39 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import reega.data.models.DataType;
 import reega.data.models.ServiceType;
 
 public class SelectiveUsageSimulator implements UsageSimulator {
 
-	private final Map<ServiceType, Generator> generators;
+    private final Map<DataType, Generator> generators;
 
-	public SelectiveUsageSimulator(List<ServiceType> services) {
-		this.generators = new HashMap<>();
-		services.forEach(srv -> this.generators.put(srv, GaussianGeneratorFactory.getGaussianGenerator(srv)));
-	}
+    public SelectiveUsageSimulator(List<DataType> services) {
+        this.generators = new HashMap<>();
+        services.forEach(srv -> this.generators.put(srv, GaussianGeneratorFactory.getGaussianGenerator(srv)));
+    }
 
-	@Override
-	public Map<ServiceType, Double> getServicesUsage() {
-		return this.getSelectedUsage(ServiceType.Categories.SERVICES);
-	}
+    @Override
+    public Map<DataType, Double> getServicesUsage() {
+        return this.getSelectedUsage(DataType.getDataTypesByService(ServiceType.GARBAGE));
+    }
 
-	@Override
-	public Map<ServiceType, Double> getWastesUsage() {
-		return this.getSelectedUsage(ServiceType.Categories.WASTES);
-	}
+    @Override
+    public Map<DataType, Double> getWastesUsage() {
+        return this.getSelectedUsage(DataType.getDataTypesByService(ServiceType.GARBAGE));
+    }
 
-	@Override
-	public Map<ServiceType, Double> getSelectedUsage(List<ServiceType> services) {
-		return services.stream().filter(generators::containsKey)
-				.collect(Collectors.toMap(srv -> srv, srv -> generators.get(srv).nextValue()));
-	}
+    @Override
+    public Map<DataType, Double> getSelectedUsage(List<DataType> services) {
+        return services.stream()
+                .filter(generators::containsKey)
+                .collect(Collectors.toMap(srv -> srv, srv -> generators.get(srv).nextValue()));
+    }
 
-	@Override
-	public Optional<Double> getUsage(ServiceType service) {
-		return Optional
-				.ofNullable(this.generators.containsKey(service) ? this.generators.get(service).nextValue() : null);
-	}
+    @Override
+    public Optional<Double> getUsage(DataType service) {
+        return Optional
+                .ofNullable(this.generators.containsKey(service) ? this.generators.get(service).nextValue() : null);
+    }
 
 }
--- src/main/java/reega/generation/UsageSimulator.java
+++ src/main/java/reega/generation/UsageSimulator.java
@@ -4,46 +4,43 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import reega.data.models.ServiceType;
+import reega.data.models.DataType;
 
 /**
  * simulates the usage of the services specified at construction.
  */
 public interface UsageSimulator {
 
-	/**
-	 * generates the usage for water, electric energy and gas, excluding the ones
-	 * NOT specified at construction. returns an empty map if no services are
-	 * specified.
-	 *
-	 * @return map of utilization values, key is the type of service.
-	 */
-	Map<ServiceType, Double> getServicesUsage();
+    /**
+     * generates the usage for water, electric energy and gas, excluding the ones NOT specified at construction. returns
+     * an empty map if no services are specified.
+     *
+     * @return map of utilization values, key is the type of service.
+     */
+    Map<DataType, Double> getServicesUsage();
 
-	/**
-	 * generates the values for paper, plastic, glass and mixed wastes, excluding
-	 * the ones NOT specified at construction. returns an empty map if no services
-	 * are specified.
-	 *
-	 * @return map of utilization values, key is the type of service.
-	 */
-	Map<ServiceType, Double> getWastesUsage();
+    /**
+     * generates the values for paper, plastic, glass and mixed wastes, excluding the ones NOT specified at
+     * construction. returns an empty map if no services are specified.
+     *
+     * @return map of utilization values, key is the type of service.
+     */
+    Map<DataType, Double> getWastesUsage();
 
-	/**
-	 * generates the values for selected services excluding the ones NOT specified
-	 * at construction. returns an empty map if no services are specified or
-	 * selected.
-	 *
-	 * @return map of utilization values, key is the type of service.
-	 */
-	Map<ServiceType, Double> getSelectedUsage(List<ServiceType> services);
+    /**
+     * generates the values for selected services excluding the ones NOT specified at construction. returns an empty map
+     * if no services are specified or selected.
+     *
+     * @return map of utilization values, key is the type of service.
+     */
+    Map<DataType, Double> getSelectedUsage(List<DataType> services);
 
-	/**
-	 * generates the usage for the specified service; if the service does not belong
-	 * to the ones specifies ate construction the method returns an Optional null.
-	 *
-	 * @param service type of service of which the usage will be generated.
-	 * @return an {@link java.util.Optional}.
-	 */
-	Optional<Double> getUsage(ServiceType service);
+    /**
+     * generates the usage for the specified service; if the service does not belong to the ones specifies ate
+     * construction the method returns an Optional null.
+     *
+     * @param data type of service of which the usage will be generated.
+     * @return an {@link java.util.Optional}.
+     */
+    Optional<Double> getUsage(DataType service);
 }
