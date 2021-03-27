de975be4a51fb96b3a8e82a942c69d6404b784af
HiiD <marco.salomone@studio.unibo.it>
2021-03-27 15:20:20 +0100

    Updated the UIAppInitializer to initialize a StatisticsController and to use the new UserMainController

--- src/main/java/reega/statistics/StatisticsController.java
+++ src/main/java/reega/statistics/StatisticsController.java
@@ -1,17 +1,23 @@
 package reega.statistics;
 
 import org.apache.commons.lang3.tuple.Pair;
+import reega.data.models.Contract;
+import reega.data.models.Data;
 import reega.data.models.ServiceType;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public interface StatisticsController {
+    void setData(List<Data> data);
     /**
      * Get the peek usage
      * @param svcType service type used to get the average usage
      * @return the date of the peek usage and the value of the peek
      */
-    Pair<Date,Double> getPeek(ServiceType svcType);
+    Optional<Pair<Date,Double>> getPeek(ServiceType svcType);
 
     /**
      * Get the average usage
