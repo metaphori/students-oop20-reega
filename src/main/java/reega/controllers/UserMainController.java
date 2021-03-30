6ec451a71e83fb2779c1a1e0a315cc08812ba49c
HiiD <marco.salomone@studio.unibo.it>
2021-03-30 10:10:07 +0200

    Delete UserMainController and UserMainControllerImpl. Modified StatisticsControllerImpl to group GARBAGE service type data by day

--- src/main/java/reega/controllers/UserMainController.java
+++
@@ -1,8 +0,0 @@
-package reega.controllers;
-
-import javafx.collections.ObservableList;
-import reega.data.models.Contract;
-
-public interface UserMainController extends MainController {
-    ObservableList<Contract> getSelectedContracts();
-}
