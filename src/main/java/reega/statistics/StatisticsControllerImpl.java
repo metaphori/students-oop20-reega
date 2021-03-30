41aaa6cabe159c4340acd9e28f1fd1132579a415
HiiD <marco.salomone@studio.unibo.it>
2021-03-30 10:10:07 +0200

    Delete UserMainController and UserMainControllerImpl. Modified StatisticsControllerImpl to group GARBAGE service type data by day

--- src/main/java/reega/statistics/StatisticsControllerImpl.java
+++ src/main/java/reega/statistics/StatisticsControllerImpl.java
@@ -1,11 +1,11 @@
 package reega.statistics;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import reega.data.DataController;
 import reega.data.models.Contract;
 import reega.data.models.Data;
 import reega.data.models.ServiceType;
-
 import java.util.*;
 import java.util.function.*;
 import java.util.stream.Collector;
@@ -25,20 +25,32 @@ public class StatisticsControllerImpl implements StatisticsController {
 
     @Override
     public Optional<Pair<Date, Double>> getPeek(ServiceType svcType) {
-        return this.data.stream().filter(data -> data.getType().getServiceType() == svcType)
-                .flatMap(data -> data.getData().entrySet().stream())
+        return groupDataByDay(svcType)
                 .max(Comparator.comparingDouble(Map.Entry::getValue))
-                .map(data -> Pair.of(new Date(data.getKey()), data.getValue()));
+                .map(data -> Pair.of(data.getKey(), data.getValue()));
     }
 
     @Override
     public double getAverageUsage(ServiceType svcType) {
-        return filterBySvcTypeAndGetData(svcType).collect(Collectors.averagingDouble(data -> data.getValue()));
+        return groupDataByDay(svcType).collect(Collectors.averagingDouble(data -> data.getValue()));
     }
 
     @Override
     public double getTotalUsage(ServiceType svcType) {
-        return filterBySvcTypeAndGetData(svcType).collect(Collectors.summingDouble(data -> data.getValue()));
+        return groupDataByDay(svcType).collect(Collectors.summingDouble(data -> data.getValue()));
+    }
+
+    private Stream<Map.Entry<Date,Double>> groupDataByDay(ServiceType svcType) {
+        // If the service type is not GARBAGE then data is produced once a day
+        // Else it's produced every hour
+        if (svcType != ServiceType.GARBAGE) {
+            return this.filterBySvcTypeAndGetData(svcType)
+                    .map(elem -> new AbstractMap.SimpleEntry<>(DateUtils.truncate(new Date(elem.getKey()), Calendar.DATE),elem.getValue()));
+        }
+        return this.filterBySvcTypeAndGetData(svcType)
+                .collect(Collectors.groupingBy(elem -> DateUtils.truncate(elem.getKey(),Calendar.DATE), Collectors.summingDouble(elem -> elem.getValue())))
+                .entrySet()
+                .stream();
     }
 
     private Stream<Map.Entry<Long,Double>> filterBySvcTypeAndGetData(ServiceType svcType) {
