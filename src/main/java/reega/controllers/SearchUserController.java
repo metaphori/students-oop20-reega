8cbaccbd2bea11f8dcc0b68cc9b9edbae5fc433b
HiiD <marco.salomone@studio.unibo.it>
2021-03-30 10:30:39 +0200

    Added some JavaDoc

--- src/main/java/reega/controllers/SearchUserController.java
+++ src/main/java/reega/controllers/SearchUserController.java
@@ -5,5 +5,10 @@ import reega.viewutils.Controller;
 import reega.viewutils.EventHandler;
 
 public interface SearchUserController extends Controller {
-    void setUserFoundEventHandler(EventHandler<User> user);
+    /**
+     * Set the user found event handler
+     * <para>The user found event handler needs to be invoked whenever a valid is user is found</para>
+     * @param userEventHandler event handler
+     */
+    void setUserFoundEventHandler(EventHandler<User> userEventHandler);
 }
--- src/main/java/reega/statistics/StatisticsController.java
+++ src/main/java/reega/statistics/StatisticsController.java
@@ -11,25 +11,29 @@ import java.util.Map;
 import java.util.Optional;
 
 public interface StatisticsController {
+    /**
+     * Set the current data
+     * @param data data used for manipulation
+     */
     void setData(List<Data> data);
     /**
      * Get the peek usage
      * @param svcType service type used to get the average usage
-     * @return the date of the peek usage and the value of the peek
+     * @return the date(day) of the peek usage and the value of the peek
      */
     Optional<Pair<Date,Double>> getPeek(ServiceType svcType);
 
     /**
-     * Get the average usage
+     * Get the average usage by day
      * @param svcType service type used to get the average usage
-     * @return the average usage
+     * @return the average usage by day
      */
     double getAverageUsage(ServiceType svcType);
 
     /**
-     * Get the total usage
+     * Get the total usage by day
      * @param svcType service type used to get the total usage
-     * @return the total usage
+     * @return the total usage by day
      */
     double getTotalUsage(ServiceType svcType);
 }
