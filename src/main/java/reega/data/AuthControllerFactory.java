c597dc576a614ebe9fb62a829dcaf63cfd0716f7
Manuele <manuele.pola@studio.unibo.it>
2021-04-02 11:00:59 +0200

    Fix data controller factory

--- src/main/java/reega/data/AuthControllerFactory.java
+++ src/main/java/reega/data/AuthControllerFactory.java
@@ -1,20 +1,13 @@
 package reega.data;
 
-import reega.data.local.LocalAuth;
 import reega.data.remote.RemoteAuthAPI;
-
-import java.sql.SQLException;
 
 /**
  * This factory returns an implementation of AuthController based on the needs.
  */
 public final class AuthControllerFactory {
-    public static AuthController getDefaultAuthController() throws ClassNotFoundException, SQLException {
+    public static AuthController getDefaultAuthController() {
         return RemoteAuthAPI.getInstance();
-    }
-
-    public static AuthController getLocalAuthController() throws ClassNotFoundException, SQLException {
-        return new LocalAuth();
     }
 
     public static AuthController getRemoteAuthController() {
