0323668ea9833008234757223333c694ddb279d8
HiiD <marco.salomone@studio.unibo.it>
2021-04-02 17:57:08 +0200

    Added getCommands() to retrieve commands and used next() call in the observable list listener

--- src/main/java/reega/controllers/MainController.java
+++ src/main/java/reega/controllers/MainController.java
@@ -3,18 +3,21 @@
  */
 package reega.controllers;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.apache.commons.lang3.tuple.Pair;
+
 import javafx.beans.property.ObjectProperty;
 import javafx.collections.ObservableList;
-import org.apache.commons.lang3.tuple.Pair;
 import reega.data.models.Contract;
 import reega.data.models.ServiceType;
 import reega.users.User;
+import reega.viewutils.Command;
 import reega.viewutils.Controller;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 /**
  * Interface for a controller that is represented by the page after the login
@@ -25,30 +28,36 @@ import java.util.Set;
 public interface MainController extends Controller {
     /**
      * Set the current user
+     *
      * @param newUser new user to set
      */
     void setUser(User newUser);
 
     /**
      * Get the current user
+     *
      * @return the current user
      */
     User getUser();
 
     /**
      * User property
+     *
      * @return the user property that contains the current user
      */
     ObjectProperty<User> user();
+
     /**
      * Get the peek usage
+     *
      * @param svcType service type used to get the average usage
      * @return the date(day) of the peek usage and the value of the peek
      */
-    Optional<Pair<Date,Double>> getPeek(ServiceType svcType);
+    Optional<Pair<Date, Double>> getPeek(ServiceType svcType);
 
     /**
      * Get the average usage
+     *
      * @param svcType service type used to get the average usage
      * @return the average usage (by day)
      */
@@ -56,6 +65,7 @@ public interface MainController extends Controller {
 
     /**
      * Get the total usage
+     *
      * @param svcType service type used to get the total usage
      * @return the total usage (by day)
      */
@@ -63,19 +73,29 @@ public interface MainController extends Controller {
 
     /**
      * Get all the available service types for the current selected contracts
+     *
      * @return a set of the available service types
      */
     Set<ServiceType> getAvailableServiceTypes();
 
     /**
      * Get the selected contracts of the {@link #user()}
+     *
      * @return the selected contracts of the {@link #user()}
      */
     ObservableList<Contract> getSelectedContracts();
 
     /**
      * Get all the contracts of the user
+     *
      * @return all the contracts of the user
      */
     List<Contract> getContracts();
+
+    /**
+     * Get all the commands available for the user
+     *
+     * @return a map containing the label as the key and the command as the value
+     */
+    Map<String, Command> getCommands();
 }
