3b8e613d192db5359edf08c7840068a04abcf1da
HiiD <marco.salomone@studio.unibo.it>
2021-04-02 10:50:24 +0200

    Added getCurrentData() method to retrieve the current data

--- src/main/java/reega/statistics/StatisticsControllerImpl.java
+++ src/main/java/reega/statistics/StatisticsControllerImpl.java
@@ -1,70 +1,80 @@
 package reega.statistics;
 
-import org.apache.commons.lang3.time.DateUtils;
-import org.apache.commons.lang3.tuple.Pair;
-import reega.data.DataController;
-import reega.data.models.Contract;
-import reega.data.models.Data;
-import reega.data.models.ServiceType;
 import java.util.*;
-import java.util.function.*;
-import java.util.stream.Collector;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import javax.inject.Inject;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import reega.data.models.Data;
+import reega.data.models.ServiceType;
 
 public class StatisticsControllerImpl implements StatisticsController {
 
     private List<Data> data;
 
-
     @Override
-    public void setData(List<Data> data) {
+    public void setData(final List<Data> data) {
         this.data = data;
     }
 
     @Override
-    public Optional<Pair<Date, Double>> getPeek(ServiceType svcType) {
-        return groupDataByDay(svcType)
+    public Optional<Pair<Date, Double>> getPeek(final ServiceType svcType) {
+        return this.groupDataByDay(svcType)
                 .max(Comparator.comparingDouble(Map.Entry::getValue))
                 .map(data -> Pair.of(data.getKey(), data.getValue()));
     }
 
     @Override
-    public double getAverageUsage(ServiceType svcType) {
-        return groupDataByDay(svcType).collect(Collectors.averagingDouble(data -> data.getValue()));
+    public double getAverageUsage(final ServiceType svcType) {
+        return this.groupDataByDay(svcType).collect(Collectors.averagingDouble(Entry::getValue));
     }
 
     @Override
-    public double getTotalUsage(ServiceType svcType) {
-        return groupDataByDay(svcType).collect(Collectors.summingDouble(data -> data.getValue()));
+    public double getTotalUsage(final ServiceType svcType) {
+        return this.groupDataByDay(svcType).collect(Collectors.summingDouble(Entry::getValue));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Data> getCurrentData() {
+        return this.data;
     }
 
     /**
      * Group the data by day
+     *
      * @param svcType service type needed for this type of data
      * @return a {@link Stream} containing pairs of Day-Value
      */
-    private Stream<Map.Entry<Date,Double>> groupDataByDay(ServiceType svcType) {
+    private Stream<Map.Entry<Date, Double>> groupDataByDay(final ServiceType svcType) {
         // If the service type is not GARBAGE then data is produced once a day
         // Else it's produced every hour
         if (svcType != ServiceType.GARBAGE) {
             return this.filterBySvcTypeAndGetData(svcType)
-                    .map(elem -> new AbstractMap.SimpleEntry<>(DateUtils.truncate(new Date(elem.getKey()), Calendar.DATE),elem.getValue()));
+                    .map(elem -> new AbstractMap.SimpleEntry<>(
+                            DateUtils.truncate(new Date(elem.getKey()), Calendar.DATE), elem.getValue()));
         }
         return this.filterBySvcTypeAndGetData(svcType)
-                .collect(Collectors.groupingBy(elem -> DateUtils.truncate(elem.getKey(),Calendar.DATE), Collectors.summingDouble(elem -> elem.getValue())))
+                .collect(Collectors.groupingBy(elem -> DateUtils.truncate(elem.getKey(), Calendar.DATE),
+                        Collectors.summingDouble(Entry::getValue)))
                 .entrySet()
                 .stream();
     }
 
     /**
      * Filter data by <code>svcType</code>
+     *
      * @param svcType svcType used for filtering data
      * @return a {@link Stream} containing pairs of TimeStamp-Value containing all the data
      */
-    private Stream<Map.Entry<Long,Double>> filterBySvcTypeAndGetData(ServiceType svcType) {
-        return this.data.stream().filter(data -> data.getType().getServiceType() == svcType)
+    private Stream<Map.Entry<Long, Double>> filterBySvcTypeAndGetData(final ServiceType svcType) {
+        return this.data.stream()
+                .filter(data -> data.getType().getServiceType() == svcType)
                 .flatMap(data -> data.getData().entrySet().stream());
     }
 }
