84a4487acf017a9d5bc7ab494c9a3ce38388f26d
HiiD <marco.salomone@studio.unibo.it>
2021-04-02 15:14:44 +0200

    Set the minWidth to 25% of the screen and minHeight according to desktop aspect ratio

--- src/main/java/reega/main/Main.java
+++ src/main/java/reega/main/Main.java
@@ -3,8 +3,12 @@
  */
 package reega.main;
 
+import com.sun.javafx.tk.Toolkit;
 import javafx.application.Application;
+import javafx.geometry.Dimension2D;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import reega.util.ServiceProvider;
 
@@ -28,7 +32,21 @@ public class Main extends Application {
     public void start(final Stage primaryStage) throws Exception {
         UIAppInitializer.getInstance().initialize();
         final ServiceProvider svcProvider = UIAppInitializer.getInstance().getServiceProvider();
+        Screen primaryScreen = Screen.getPrimary();
+        double aspectRatio = getScreenAspectRatio(primaryScreen);
+        Rectangle2D bounds = primaryScreen.getVisualBounds();
+        //Use the 75% of the width
+        double minWidth = bounds.getWidth() * 0.25;
+        //Keep the 16/9 proportion
+        double minHeight = minWidth * (1/aspectRatio);
+        primaryStage.setMinHeight(minHeight);
+        primaryStage.setMinWidth(minWidth);
         primaryStage.setScene(new Scene(svcProvider.getRequiredService(BaseLayoutView.class)));
         primaryStage.show();
+    }
+
+    private double getScreenAspectRatio(Screen screen) {
+        Rectangle2D bounds = screen.getBounds();
+        return bounds.getWidth() / bounds.getHeight();
     }
 }
