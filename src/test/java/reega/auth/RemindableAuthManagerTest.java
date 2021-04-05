d4a8afb577aeb364809100ac2121310282124780
HiiD <marco.salomone@studio.unibo.it>
2021-04-05 17:13:56 +0200

    Modified RemindableAuthManagerTest

--- src/test/java/reega/auth/RemindableAuthManagerTest.java
+++ src/test/java/reega/auth/RemindableAuthManagerTest.java
@@ -6,9 +6,14 @@ package reega.auth;
 import okhttp3.mockwebserver.Dispatcher;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import reega.data.AuthControllerFactory;
+import reega.data.DataController;
+import reega.data.DataControllerFactory;
 import reega.data.mock.MockConnection;
 import reega.data.mock.MockedAuthService;
 import reega.data.mock.RequestDispatcher;
+import reega.data.mock.TestConnection;
+import reega.data.remote.RemoteConnection;
 import reega.io.IOControllerFactory;
 import reega.io.MockIOController;
 import reega.io.TokenIOController;
@@ -27,33 +32,28 @@ import java.util.Optional;
 @TestInstance(Lifecycle.PER_CLASS)
 class RemindableAuthManagerTest {
 
-    private static MockConnection connection;
-    private static MockedAuthService authService;
-    private static TokenIOController ioController;
-    private static AuthManager authManager;
+    private RemoteConnection connection;
+    private TokenIOController ioController;
+    private AuthManager authManager;
 
     @BeforeAll
-    static void setup() throws IOException {
-        RemindableAuthManagerTest.authService = new MockedAuthService();
-        final Dispatcher dispatcher = new RequestDispatcher(null, RemindableAuthManagerTest.authService);
-        RemindableAuthManagerTest.connection = new MockConnection(dispatcher);
-        RemindableAuthManagerTest.ioController = IOControllerFactory.createTokenIOController(new MockIOController());
-        RemindableAuthManagerTest.authManager = new RemindableAuthManager(
-                RemindableAuthManagerTest.connection.getAuthAPI(), new MockExceptionHandler(),
-                RemindableAuthManagerTest.ioController);
+    public void setup() throws IOException {
+        connection = new TestConnection().getTestConnection("admin@reega.it", "AES_PASSWORD");
+        ioController = IOControllerFactory.createTokenIOController(new MockIOController());
+        authManager = new RemindableAuthManager(AuthControllerFactory.getRemoteAuthController(connection),new MockExceptionHandler(), ioController);
+        authManager.logout();
+        connection.logout();
     }
 
     @AfterAll
-    static void cleanup() throws IOException {
-        RemindableAuthManagerTest.connection.close();
-
+    public void cleanup() throws IOException {
+        connection.getService().terminateTest().execute();
     }
 
     @AfterEach
     void cleanUsers() {
-        RemindableAuthManagerTest.authService.clean();
         // Delete the token file if it exists
-        final File tokenFile = new File(RemindableAuthManagerTest.ioController.getTokenFilePath());
+        final File tokenFile = new File(this.ioController.getTokenFilePath());
         if (tokenFile.exists()) {
             tokenFile.delete();
         }
@@ -62,67 +62,71 @@ class RemindableAuthManagerTest {
     @Test
     public void addUserAndLoginTest() {
         // Try to login with email or fiscal code, without having created credentials
-        Optional<GenericUser> u = RemindableAuthManagerTest.authManager.emailLogin("test@reega.it", "PASSWORD", false);
+        Optional<GenericUser> u = this.authManager.emailLogin("test@reega.it", "PASSWORD", false);
         Assertions.assertTrue(u.isEmpty());
-        u = RemindableAuthManagerTest.authManager.fiscalCodeLogin("ZZZ999", "PASSWORD", false);
+        u = this.authManager.fiscalCodeLogin("ZZZ999", "PASSWORD", false);
         Assertions.assertTrue(u.isEmpty());
 
         // Create a new user and try an email login
-        NewUser newUser = new NewUser(Role.USER, "test", "surname", "test@reega.it", "ABC123", "PASSWORD");
-        RemindableAuthManagerTest.authManager.createUser(newUser);
-        u = RemindableAuthManagerTest.authManager.emailLogin("test@reega.it", "PASSWORD", false);
+        NewUser newUser = new NewUser(Role.USER, "test", "surname", "addusertest1@reega.it", "AUT1", "PASSWORD");
+        this.authManager.createUser(newUser);
+        u = this.authManager.emailLogin("addusertest1@reega.it", "PASSWORD", false);
         Assertions.assertTrue(u.isPresent());
+        connection.logout();
 
         // Create a new user and try a fiscal code login
-        newUser = new NewUser(Role.USER, "test", "surname", "test@reega.it", "ZZZ999", "PASSWORD");
-        RemindableAuthManagerTest.authManager.createUser(newUser);
-        u = RemindableAuthManagerTest.authManager.fiscalCodeLogin("ZZZ999", "PASSWORD", false);
+        newUser = new NewUser(Role.USER, "test", "surname", "addusertest2@reega.it", "AUT2", "PASSWORD");
+        this.authManager.createUser(newUser);
+        u = this.authManager.fiscalCodeLogin("AUT2", "PASSWORD", false);
         Assertions.assertTrue(u.isPresent());
+        connection.logout();
     }
 
     @Test
     public void tokenLoginTest() {
         // Create two users
-        final NewUser newUser1 = new NewUser(Role.USER, "test1", "surname1", "test1@reega.it", "ABC123", "PASSWORD");
-        final NewUser newUser2 = new NewUser(Role.USER, "test1", "surname2", "test2@reega.it", "ZZZ999", "PASSWORD");
-        // Set the user IDs
-        RemindableAuthManagerTest.authService.setUserID(1);
-        RemindableAuthManagerTest.authManager.createUser(newUser1);
-
-        RemindableAuthManagerTest.authService.setUserID(2);
-        RemindableAuthManagerTest.authManager.createUser(newUser2);
+        final NewUser newUser1 = new NewUser(Role.USER, "test1", "surname1", "tokentest1@reega.it", "TKL1", "PASSWORD");
+        final NewUser newUser2 = new NewUser(Role.USER, "test1", "surname2", "tokentest2@reega.it", "TKL2", "PASSWORD");
+        this.authManager.createUser(newUser1);
+        this.authManager.createUser(newUser2);
 
         // Login the second user and save the token
-        Optional<GenericUser> usr = RemindableAuthManagerTest.authManager.emailLogin("test2@reega.it", "PASSWORD",
+        this.authManager.emailLogin("tokentest2@reega.it", "PASSWORD",
                 true);
 
+        this.connection.logout();
+
         // Try the login without password
-        usr = RemindableAuthManagerTest.authManager.tryLoginWithoutPassword();
+        Optional<GenericUser> usr = this.authManager.tryLoginWithoutPassword();
         Assertions.assertTrue(usr.isPresent());
         Assertions.assertEquals(newUser2.getEmail(), usr.get().getEmail());
+        this.authManager.logout();
+        this.connection.logout();
     }
 
     @Test
     public void logoutTest() {
         // Create a new user
-        final NewUser newUser = new NewUser(Role.USER, "test", "surname", "test@reega.it", "ABC123", "PASSWORD");
-        RemindableAuthManagerTest.authManager.createUser(newUser);
+        final NewUser newUser = new NewUser(Role.USER, "test", "surname", "logouttest@reega.it", "LT1", "PASSWORD");
+        this.authManager.createUser(newUser);
         // Login and store the token
-        RemindableAuthManagerTest.authManager.emailLogin("test@reega.it", "PASSWORD", true);
-
-        Optional<GenericUser> user = RemindableAuthManagerTest.authManager.tryLoginWithoutPassword();
+        this.authManager.emailLogin("logouttest@reega.it", "PASSWORD", true);
+        this.connection.logout();
+        Optional<GenericUser> user = this.authManager.tryLoginWithoutPassword();
         Assertions.assertTrue(user.isPresent());
 
         // Logout (so that if a token exists, it gets deleted)
-        RemindableAuthManagerTest.authManager.logout();
+        this.authManager.logout();
+        this.connection.logout();
 
         // Try the login without password
-        user = RemindableAuthManagerTest.authManager.tryLoginWithoutPassword();
+        user = this.authManager.tryLoginWithoutPassword();
         Assertions.assertTrue(user.isEmpty());
 
         // Try the login with email
-        user = RemindableAuthManagerTest.authManager.emailLogin("test@reega.it", "PASSWORD", false);
+        user = this.authManager.emailLogin("logouttest@reega.it", "PASSWORD", false);
         Assertions.assertTrue(user.isPresent());
+        this.connection.logout();
     }
 
 }
