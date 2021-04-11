32907762919abd960eadc5a2483e42ed068f14d4
Manuele <manuele.pola@studio.unibo.it>
2021-04-11 11:48:38 +0200

    Add tests for FC validator

--- src/main/java/reega/util/FiscalCodeValidator.java
+++ src/main/java/reega/util/FiscalCodeValidator.java
@@ -16,24 +16,25 @@ public final class FiscalCodeValidator {
     private static FiscalCodeValidator instance;
     private static final Map<Character, Integer> oddIntegersFromChars;
     private static final Map<Character, Integer> evenIntegersFromChars;
-    private static Pattern fiscalCodePattern = Pattern.compile("([A-Z]){6}([0-9]){2}([A-Z]){1}([0-9]){2}([0-9A-Z]){4}");
+    private static Pattern fiscalCodePattern = Pattern.compile("([A-Z]){6}([0-9]){2}([A-Z])([0-9]){2}([0-9A-Z]){4}");
 
     static {
         // Get all the possible letters and numbers for a fiscal code
         final char[] lettersAndNumbers = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
         // Get all the values associated with each alphanumeric character (in order),
         // when the character of the fiscal code is in a odd index
-        final int[] oddIntegerValues = { 1, 0, 5, 7, 9, 13, 15, 17, 19, 21, 1, 0, 5, 7, 9, 13, 15, 17, 19, 21, 2, 4, 18,
-                20, 11, 3, 6, 8, 12, 14, 16, 10, 22, 25, 24, 23 };
+        final int[] oddIntegerValues = {1, 0, 5, 7, 9, 13, 15, 17, 19, 21, 1, 0, 5, 7, 9, 13, 15, 17, 19, 21, 2, 4, 18,
+                20, 11, 3, 6, 8, 12, 14, 16, 10, 22, 25, 24, 23};
         // Get all the values associated with each alphanumeric character (in order),
         // when the character of the fiscal code is in a even index
-        final int[] eventIntegerValues = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-                14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+        final int[] evenIntegerValues = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+                14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
         oddIntegersFromChars = new HashMap<>(lettersAndNumbers.length);
         evenIntegersFromChars = new HashMap<>(lettersAndNumbers.length);
         for (int i = 0; i < lettersAndNumbers.length; i++) {
             FiscalCodeValidator.oddIntegersFromChars.put(lettersAndNumbers[i], oddIntegerValues[i]);
-            FiscalCodeValidator.evenIntegersFromChars.put(lettersAndNumbers[i], eventIntegerValues[i]);
+            FiscalCodeValidator.evenIntegersFromChars.put(lettersAndNumbers[i], evenIntegerValues[i]);
         }
     }
 
