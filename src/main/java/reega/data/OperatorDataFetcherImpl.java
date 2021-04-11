750236056db441a1ceaa40cde8a8ceade61f9e1b
HiiD <marco.salomone@studio.unibo.it>
2021-04-11 15:06:27 +0200

    Created data fetchers for the MainController and the OperatorMainController. Also created contract fetcher to fetch all contracts of the user

---
+++ src/main/java/reega/data/OperatorDataFetcherImpl.java
@@ -0,0 +1,27 @@
+package reega.data;
+
+import reega.data.models.Contract;
+import reega.data.models.Data;
+import reega.logging.ExceptionHandler;
+import reega.users.User;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+public class OperatorDataFetcherImpl extends DataFetcherImpl implements OperatorDataFetcher {
+
+    public OperatorDataFetcherImpl(DataController dataController, ExceptionHandler exceptionHandler) {
+        super(dataController, exceptionHandler);
+    }
+
+    @Override
+    public List<Data> getGeneralData() {
+        try {
+            return this.getDataController().getMonthlyData(null);
+        } catch (IOException e) {
+            this.getExceptionHandler().handleException(e);
+            return Collections.emptyList();
+        }
+    }
+}
