582f6696b886e40ab3f98c00512ebb380052274a
Roberto Casadei <robyonrails@gmail.com>
2021-05-12 09:53:35 +0200

    oop machinery

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import de.aaschmid.gradle.plugins.cpd.Cpd
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
@@ -6,13 +7,40 @@ plugins {
     id("jacoco")
     id("application")
     id("eclipse")
+    checkstyle
+    pmd
+    id("de.aaschmid.cpd")
+    id("com.github.spotbugs")
+    `build-dashboard`
 }
+
+/*
+sourceSets {
+    main {
+        java {
+            srcDirs("src")
+        }
+        resources {
+            srcDirs("res")
+        }
+    }
+    test {
+        java {
+            srcDirs("src")
+        }
+    }
+}
+*/
 
 repositories {
     mavenCentral()
     jcenter()
     maven(url = "https://plugins.gradle.org/m2/")
+    flatDir {
+        dirs("lib")
+    }
 }
+
 
 version = "1.0.0"
 java.sourceCompatibility = JavaVersion.VERSION_11
@@ -29,20 +57,24 @@ jacoco {
 
 val javaFXModules = listOf("base", "controls", "fxml", "graphics", "swing")
 val supportedPlatforms = listOf("linux", "mac", "win")
+val javaFxVersion = "17-ea+3"
 
 dependencies {
     for (platform in supportedPlatforms) {
         for (module in javaFXModules) {
-            implementation("org.openjfx:javafx-$module:17-ea+3:$platform")
+            implementation("org.openjfx:javafx-$module:$javaFxVersion:$platform")
         }
     }
 
     // Tests
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    implementation("junit:junit:_")
+    runtimeOnly("org.junit.vintage:junit-vintage-engine:_")
+
     testImplementation("com.squareup.okhttp3:mockwebserver:4.9.0")
-    
-    // Implementation to make MockWebServer work with JUnit 5 in Eclipse 
+
+    // Implementation to make MockWebServer work with JUnit 5 in Eclipse
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
 
     // HTTP client library - stick to 2.7.0 to avoid illegal reflections (should upgrade to Java 14)
@@ -70,6 +102,13 @@ dependencies {
 
     // Email validation
     implementation("commons-validator:commons-validator:1.7")
+
+    File("lib")
+        .takeIf { it.exists() }
+        ?.takeIf { it.isDirectory }
+        ?.listFiles()
+        ?.filter { it.extension == "jar" }
+        ?.forEach { implementation(files("lib/${it.name}")) }
 }
 
 defaultTasks("clean", "shadowJar")
@@ -93,4 +132,55 @@ tasks {
         archiveFileName.set("reega.jar")
         mergeServiceFiles()
     }
+}
+
+/*
+tasks.withType<Test>() {
+    ignoreFailures = true
+    useJUnitPlatform()
+}
+*/
+
+spotbugs {
+    setEffort("max")
+    setReportLevel("low")
+    showProgress.set(true)
+    val excludeFile = File("${project.rootProject.projectDir}/config/spotbugs/excludes.xml")
+    if (excludeFile.exists()) {
+        excludeFilter.set(excludeFile)
+    }
+}
+
+tasks.withType<com.github.spotbugs.snom.SpotBugsTask> {
+    ignoreFailures = true
+    reports {
+        create("html") {
+            enabled = true
+        }
+    }
+}
+
+pmd {
+    ruleSets = listOf()
+    ruleSetConfig = resources.text.fromFile("${project.rootProject.projectDir}/config/pmd/pmd.xml")
+    isIgnoreFailures = true
+}
+
+cpd {
+    isIgnoreFailures = true
+}
+
+tasks.withType<Cpd> {
+    reports {
+        xml.setEnabled(false)
+        text.setEnabled(true)
+    }
+    language = "java"
+    minimumTokenCount = 50
+    ignoreFailures = true
+    source = sourceSets["main"].allJava
+}
+
+checkstyle {
+    isIgnoreFailures = true
 }
